1de38cefcaefae893ccae7094d0c5494eb63ceaf
e_sibuya@outlook.com <*e_sibuya@outlook.com*>
2023-06-15 15:55:15 -0300

    commit and push

--- src/styles.ts
+++ src/styles.ts
@@ -17,8 +17,8 @@ export const GlobalCss = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Roboto', sans-serif;
     list-style: none;
+    font-family: 'Roboto', sans-serif;
   }
 
   body {
